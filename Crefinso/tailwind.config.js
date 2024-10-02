2eae73639784d74e7f6acf3680e27765a571531e
Christopher Villalta <va23003@esfe.agape.edu.sv>
2024-10-01 22:46:13 -0600

    update all project

---
+++ Crefinso/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    purge: {
+        enabled: true,
+        content: [
+            './**/*.html',
+            './**/*.razor'
+        ],
+    },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
