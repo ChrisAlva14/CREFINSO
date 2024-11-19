1862771b7938fc5f6e06fc53ec6b6af2583309ea
Christopher Villalta <va23003@esfe.agape.edu.sv>
2024-11-18 22:47:36 -0600

    Updating the all project

--- Crefinso/Services/Clientes/ClientServices.cs
+++ Crefinso/Services/Clientes/ClientServices.cs
@@ -14,6 +14,7 @@ namespace Crefinso.Services.Clientes
             _authServices = authServices;
         }
 
+        //OBTENER TODOS LOS CLIENTES
         public async Task<List<ClienteResponse>> GetClientes()
         {
             try
@@ -39,6 +40,46 @@ namespace Crefinso.Services.Clientes
                 throw new Exception("HA OCURRIDO UN ERROR AL OBTENER LOS CLIENTES, POR FAVOR REINICIAR EL SISTEMA");
             }
 
+        }
+
+        //CREAR NUEVO CLIENTE
+        public async Task<bool> PostClient(ClienteRequest newClient)
+        {
+            try
+            {
+                // Obtener el token de autenticación
+                var token = await _authServices.GetToken();
+                if (string.IsNullOrEmpty(token))
+                {
+                    throw new InvalidOperationException("TOKEN INVALIDO O NULO, POR FAVOR, INICIAR SESIÓN");
+                }
+
+                // Agregar el token al encabezado de autorización
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                // Enviar la solicitud POST con los datos del nuevo usuario
+                var response = await _httpClient.PostAsJsonAsync("api/clientes", newClient);
+
+                // Verificar si la respuesta fue exitosa
+                if (response.IsSuccessStatusCode)
+                {
+                    return true; // Indica que la creación fue exitosa
+                }
+                else
+                {
+                    // Manejar errores de la respuesta
+                    var errorMessage = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Error al crear el Cliente. Código de estado: {response.StatusCode}. Detalle: {errorMessage}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception("ERROR EN LA SOLICITUD HTTP: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("HA OCURRIDO UN ERROR AL CREAR EL USUARIO, POR FAVOR REINICIAR EL SISTEMA. Detalle: " + ex.Message);
+            }
         }
     }
 }
